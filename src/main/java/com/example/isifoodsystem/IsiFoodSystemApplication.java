1d66f57336817583d28c619c8f4f58539e1b5909
ostrowskaew <ostrowskaew@gmail.com>
2022-04-04 20:29:15 +0200

    first commit

---
+++ src/main/java/com/example/isifoodsystem/IsiFoodSystemApplication.java
@@ -0,0 +1,20 @@
+package com.example.isifoodsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class IsiFoodSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsiFoodSystemApplication.class, args);
+    }
+
+
+
+
+
+}
---
+++ src/test/java/com/example/isifoodsystem/IsiFoodSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.isifoodsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsiFoodSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
